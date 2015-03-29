e79e789324b417859efe5ef9ef269314ff250069
Alex Haas <alex.carl.haas@gmail.com>
2015-03-28 20:54:56 -0400

    complete path creator

--- word_chains.rb
+++ word_chains.rb
@@ -24,28 +24,56 @@ class WordChainer
 
   def run(source, target)
     @current_words = [source]
-    @all_seen_words = [source]
+    @all_seen_words = {source => nil}
 
     until @current_words.empty?
       explore_current_words
     end
+
+    p build_path(target)
   end
 
   def explore_current_words
     new_current_words = []
 
-    @current_words.each do |word|
-      adjacent_words(word).each do |adjacent_word|
-        next if @all_seen_words.include?(adjacent_word)
+    @current_words.each do |current_word|
+      adjacent_words(current_word).each do |adjacent_word|
+        next if @all_seen_words.keys.include?(adjacent_word)
         new_current_words << adjacent_word
-        @all_seen_words << adjacent_word
+        @all_seen_words[adjacent_word] = current_word
       end
     end
 
-    puts new_current_words
     @current_words = new_current_words
   end
 
+  def build_path(target)
+    path = [target]
+    next_step = target
+    until @all_seen_words[next_step].nil?
+      path << @all_seen_words[next_step]
+      next_step = @all_seen_words[next_step]
+    end
+    path
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Where do you want to start?"
+  source = gets.chomp.downcase
+  target = ""
+
+  until target.length == source.length
+    puts"Where do you want to end?"
+    target = gets.chomp.downcase
+    unless target.length == source.length
+      puts "Please choose equal length words"
+    end
+  end
+
+  chain = WordChainer.new('dictionary.txt')
+  chain.run(source, target)
 
 
 end
