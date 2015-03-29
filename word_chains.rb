ddf4d4303fbb6df469de7746f7e554a10267e0ab
Alex Haas <alex.carl.haas@gmail.com>
2015-03-28 20:02:59 -0400

    complete run method

--- word_chains.rb
+++ word_chains.rb
@@ -1,9 +1,47 @@
-include 'set'
+require 'set'
 
 class WordChainer
 
   def initialize(dictionary_file_name)
     @dictionary = Set.new(File.readlines(dictionary_file_name).map(&:chomp))
   end
+
+  def adjacent_words(word)
+    @dictionary.select do |word_check|
+      one_off?(word, word_check)
+    end
+  end
+
+  def one_off?(base_word, word)
+    return false unless word.length == base_word.length
+    count = 0
+    word.length.times do |idx|
+      count += 1 unless word[idx] == base_word[idx]
+    end
+    count == 1
+  end
+
+
+  def run(source, target)
+    @current_words = [source]
+    @all_seen_words = [source]
+
+    until @current_words.empty?
+      new_current_words = []
+
+      @current_words.each do |word|
+        adjacent_words(word).each do |adjacent_word|
+          next if @all_seen_words.include?(adjacent_word)
+          new_current_words << adjacent_word
+          @all_seen_words << adjacent_word
+        end
+      end
+
+      puts new_current_words
+      @current_words = new_current_words
+    end
+  end
+
+
 
 end
